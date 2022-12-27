066b5a0ef6e1b11bf5f53b84ce4a3c722dcaaa08
Anja Papatola <mamadev@apalopta.de>
2022-12-27 23:16:46 +0100

    solve day 25 part 1

--- src/Day25.kt
+++ src/Day25.kt
@@ -1,3 +1,4 @@
+import kotlin.math.pow
 
 fun main() {
     val input = readInput("Day25")
@@ -5,6 +6,46 @@ fun main() {
     part1(input).println()
 }
 
-private fun part1(input: List<String>): Int {
-    return(input.size)
+private fun part1(input: List<String>): String {
+    return input.sumOf { line -> line.toDec() }.toSnafu()
+}
+
+infix fun Int.pow(i: Int) = toDouble().pow(i.toDouble()).toLong()
+
+val snafuToDecMap = mapOf(
+    '1' to 1,
+    '2' to 2,
+    '0' to 0,
+    '-' to -1,
+    '=' to -2
+)
+
+val decToSnafuMap = mapOf(
+    -2 to "=",
+    -1 to "-",
+    2 to "2",
+    1 to "1",
+    0 to "0",
+)
+
+fun String.toDec(): Long =
+    toCharArray().reversed()
+        .foldIndexed(0L) { exp, acc, char ->
+            if (char !in snafuToDecMap.keys) {
+                error("unknown snafu $char in $this")
+            }
+            (acc + snafuToDecMap[char]!! * (5 pow exp))
+        }
+
+fun Long.toSnafu(): String {
+    var value = this
+    var charsReversed = ""
+    while (value > 0) {
+        val current = (value % 5).toInt()
+        val remainder = (if (current > 2) (current - 5) else 0).toInt()
+        charsReversed += if (current > 2) decToSnafuMap[remainder] else decToSnafuMap[current]
+        val trans = (if (current > 2) -remainder else 0)
+        value = ((value + trans) / 5)
+    }
+    return charsReversed.reversed()
 }
---
+++ src/Day25_test.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val inputToDec = readInput("Day25_test_toDec").drop(1)
+        .map { it.trim().split(" +".toRegex()) }
+        .associate { (k, v) -> k to v.toLong() }
+    testToDec(inputToDec)
+
+    val inputToSnafu = readInput("Day25_test_toSnafu").drop(1)
+        .map { it.trim().split(" +".toRegex()) }
+        .associate { (k, v) -> k.trim().toLong() to v.trim() }
+    testToSnafu(inputToSnafu)
+}
+
+private fun testToDec(input: Map<String, Long>) {
+    input.forEach { (snafu, dec) ->
+        val calc = snafu.toDec()
+        if (calc != dec) error("fail '$calc' from '$snafu'")
+    }
+}
+
+private fun testToSnafu(input: Map<Long,String>) {
+    input.forEach { (dec, snafu) ->
+        val calc = dec.toSnafu()
+        if (calc != snafu) error("fail '$calc' to '$snafu'")
+    }
+}
